d6bc9034558cfe1de94ec017baab261b7de5eef8
Ademar Fatama Ruiz <mfatama@soaint.com>
2026-06-09 01:21:40 -0500

    fix: set correct baseUrl and trailingSlash for GCS static hosting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,9 +16,8 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://storage.googleapis.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/thesis-rag-poc-thesis-doc-test-2/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
